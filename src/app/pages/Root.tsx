f2e5163ab8507c8bed893ff6db852442575028ae
bienvenudev <cbienvenu007@gmail.com>
2026-04-04 22:47:15 +0200

    feat: add ScrollToTop component for automatic scrolling on route change

--- src/app/pages/Root.tsx
+++ src/app/pages/Root.tsx
@@ -1,10 +1,20 @@
-import { Outlet } from 'react-router';
+import { useEffect } from 'react';
+import { Outlet, useLocation } from 'react-router';
 import { Navigation } from '../components/Navigation';
 import { Footer } from '../components/Footer';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'auto' });
+  }, [pathname]);
+  return null;
+}
 
 export function Root() {
   return (
     <div className="min-h-screen bg-white">
+      <ScrollToTop />
       <Navigation />
       <Outlet />
       <Footer />
